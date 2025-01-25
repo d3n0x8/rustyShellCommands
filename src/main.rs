24e607264682748cdbb91eddef6eacc3e6176fd7
Andreas Rey Malissein <andreasrey87@gmail.com>
2025-01-25 19:03:56 +0100

    add arguments for ls command

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ use std::fs::*;
 #[derive(Parser)]
 struct Parameters {
     param: String,
+
+    #[arg(short, long)]
+    all: bool,
+
+    #[arg(default_value_t = String::from("."))]
+    argum: String,
 }
 
 fn main() {
@@ -12,22 +18,28 @@ fn main() {
     let commande = &args.param;
 
     match commande.as_str() {
-        "ls" => ls(),
-        _ => println!("Command not found"),
+        "ls" => ls(args.all, args.argum),
+        _ => println!("Command not found."),
     };
 }
 
-fn ls() {
-    let path = ".";
-    match read_dir(path) {
-        Ok(dir) => {
-            for file in dir {
-                match file {
-                    Ok(file) => print!("{}  ", file.path().display()),
-                    Err(e) => println!("Error reading: {}", e),
+fn ls(all: bool, argum: String) {
+    let path = argum;
+    if let Ok(dir) = read_dir(path) {
+        for file in dir {
+            if let Ok(file) = file {
+                if let Some(file_name) = file.file_name().to_str() {
+                    if all || !file_name.starts_with('.') {
+                        print!("{}  ", file_name);
+                    }
+                } else {
+                    println!("Error : Failed to convert name to string.");
                 }
+            } else {
+                println!("Error reading");
             }
         }
-        Err(e) => println!("Error reading directory: {}", e),
+    } else {
+        println!("Error reading directory");
     }
 }
